cf1bc1334f4c9bfd7a8bb5f1ecfa9abd17a879df
Maximiliano Nicolás Güelpa <mguelpa@gmail.com>
2020-10-04 20:59:37 -0300

    adding fontawesome-free

--- mguelpa-lab-iv-ejercitacion-preparcial/src/app/shared/top-nav/top-nav.component.ts
+++ mguelpa-lab-iv-ejercitacion-preparcial/src/app/shared/top-nav/top-nav.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TopNavComponent implements OnInit {
 
+  public logged: boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
